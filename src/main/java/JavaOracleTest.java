94fa0f666400ab617c960ae079c129f0e76a029b
Gabriel <vgabrielcv@gmail.com>
2020-11-24 00:17:45 -0500

    Conexion con bd hecha

---
+++ src/main/java/JavaOracleTest.java
@@ -0,0 +1,39 @@
+import java.sql.*;
+
+public class JavaOracleTest {
+    public static void main(String[] args) {
+                        //jdbc:oracle:thin:@localhost:1521:orcl
+        String dbUrl = "jdbc:oracle:thin:@152.67.62.154:1521/bdulima1_pdb1.subnetpub.vcninfra.oraclevcn.com";
+        String username = "alumno12";
+        String password = "PeruLima__2020";
+
+        try {
+            Connection connection = DriverManager.getConnection(dbUrl, username, password);
+            System.out.println("Connected to Oracle database server");
+            String sql = "INSERT INTO AREA VALUES (?, ?)";
+            PreparedStatement statement = connection.prepareStatement(sql);
+            statement.setString(1, "19");
+            statement.setString(2, "asdaasdw");
+            int rows = statement.executeUpdate();
+
+            if(rows > 0 ){
+                System.out.println("A row has been inserted.");
+            }
+            statement.close();
+
+            String sql2 = "SELECT * FROM AREA";
+            Statement statement2 = connection.createStatement();
+            ResultSet result = statement2.executeQuery(sql2);
+
+            while(result.next()){
+                String id = result.getString(1);
+                String name = result.getString("nombre");
+                System.out.println("ID: " + id + " Name: " + name);
+            }
+
+            connection.close();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+}
